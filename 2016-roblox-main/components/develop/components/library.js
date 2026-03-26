8075b9b90e946ee8bccb78e476b83d6edb5f056d
Unknown <unknownwasd5@gmail.com>
2026-03-26 18:31:52 +0300

    fixed stuf

--- 2016-roblox-main/components/develop/components/library.js
+++ 2016-roblox-main/components/develop/components/library.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
+import CatalogPageStore from "../../../stores/libraryPage";
 import LibraryInput from "./library/libraryInput";
 import LibraryNavigation from "./library/libraryNavigation";
 import LibraryResults from "./library/libraryResults";
@@ -13,26 +14,28 @@ const useStyles = createUseStyles({
 })
 
 const LibraryPage = props => {
-    return <div>
-        <div className='row mt-2'>
-            <div className='col-12 col-md-8 col-lg-10' style={{marginLeft: 'auto'}}>
-                <LibraryInput />
-            </div>
-        </div>
-        <div className='row'>
-            <div className='col-12 col-md-4 col-lg-2'>
-                <div className='divider-right'>
-                    <div className='pe-2'>
-                        <LibraryNavigation />
-                        <LibraryFilters />
-                    </div>
+    return <CatalogPageStore.Provider>
+        <div>
+            <div className='row mt-2'>
+                <div className='col-12 col-md-8 col-lg-10' style={{ marginLeft: 'auto' }}>
+                    <LibraryInput />
                 </div>
             </div>
-            <div className='col-12 col-md-8 col-lg-10'>
-                <LibraryResults />
+            <div className='row'>
+                <div className='col-12 col-md-4 col-lg-2'>
+                    <div className='divider-right'>
+                        <div className='pe-2'>
+                            <LibraryNavigation />
+                            <LibraryFilters />
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-8 col-lg-10'>
+                    <LibraryResults />
+                </div>
             </div>
         </div>
-    </div>
+    </CatalogPageStore.Provider>
 }
 
 export default LibraryPage;
